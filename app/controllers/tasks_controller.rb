5dfe93d33d84003559da8eb05241a91f31c16e1a
yoshito <yoshito5211@gmail.com>
2021-08-28 04:04:19 +0900

    createアクションを実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,5 +7,20 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    @task = Task.new(task_params)
+    if @task.save!
+      redirect_to root_path, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
   def edit; end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
 end
