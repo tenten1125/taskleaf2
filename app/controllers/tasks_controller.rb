f746670658095c82efab6afe4661e3bd72885c47
yoshito <yoshito5211@gmail.com>
2021-08-28 05:05:06 +0900

    タスク編集機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,7 +20,18 @@ class TasksController < ApplicationController
     end
   end
 
-  def edit; end
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    if task.update!(task_params)
+      redirect_to task, notice: "タスク「#{task.name}」を更新しました。"
+    else
+      render :edit
+    end
+  end
 
   private
 
