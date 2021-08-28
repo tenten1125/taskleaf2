b9db1534ab9d6409910b76b5a611be9691aef1f6
yoshito <yoshito5211@gmail.com>
2021-08-28 17:24:45 +0900

    tasksコントローラーをリファクタリング

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+
   def index
-    @tasks = current_user.tasks
+    @tasks = current_user.tasks.recent
   end
 
-  def show
-    @task = current_user.tasks.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -20,28 +20,28 @@ class TasksController < ApplicationController
     end
   end
 
-  def edit
-    @task = current_user.tasks.find(params[:id])
-  end
+  def edit; end
 
   def update
-    task = current_user.tasks.find(params[:id])
-    if task.update!(task_params)
-      redirect_to task, notice: "タスク「#{task.name}」を更新しました。"
+    if @task.update!(task_params)
+      redirect_to task, notice: "タスク「#{@task.name}」を更新しました。"
     else
       render :edit
     end
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
-    task.destroy!
-    redirect_to root_path, notice: "タスク「#{task.name}」を削除しました。"
+    @task.destroy!
+    redirect_to root_path, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
   private
 
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
-  
+
+  scope :recent, -> { order(created_at: :desc) }
+
   validates :name, presence: true, length: { maximum: 30 }
 end
