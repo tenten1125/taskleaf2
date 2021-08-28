ccefc871ae1d672cddf7bdffbe99314f48cb91fd
yoshito <yoshito5211@gmail.com>
2021-08-28 10:44:11 +0900

    Taskのnameにバリデーションを設定し、ビューに検証エラーを表示可能に

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,8 +13,8 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    if @task.save!
-      redirect_to root_path, notice: "タスク「#{@task.name}」を登録しました。"
+    if @task.save
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
     end
